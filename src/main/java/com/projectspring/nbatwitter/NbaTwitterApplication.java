209002cb8ecc0fe3d5060653cd50f92a5a72aabd
Suril Shah <suril.shah.94@gmail.com>
2018-03-21 18:40:07 -0700

    skeleton of project

---
+++ src/main/java/com/projectspring/nbatwitter/NbaTwitterApplication.java
@@ -0,0 +1,12 @@
+package com.projectspring.nbatwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaTwitterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/projectspring/nbatwitter/NbaTwitterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.projectspring.nbatwitter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NbaTwitterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
