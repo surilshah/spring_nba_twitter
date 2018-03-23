4bc99f6dce3f7a69ad96431d9cff698456ca4105
Suril Shah <suril.shah.94@gmail.com>
2018-03-22 23:41:27 -0700

    Created the Player object

---
+++ src/main/java/com/projectspring/nbatwitter/Player.java
@@ -0,0 +1,39 @@
+package com.projectspring.nbatwitter;
+
+public class Player {
+
+    private String id, lastName, firstName, twitter;
+
+    public void setId(String playerID) {
+        this.id = playerID;
+    }
+
+    public void setLastName(String playerLastName) {
+        this.lastName = playerLastName;
+    }
+
+    public void setFirstName(String playerFirstName) {
+        this.firstName = playerFirstName;
+    }
+
+    public void setTwitter(String playerTwitter) {
+        this.twitter = playerTwitter;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getTwitter() {
+        return this.twitter;
+    }
+
+}
